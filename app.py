2df28d9f4f8e170ea98cfca835f95d75151cb957
Vincent <makokhavomondi@gmail.com>
2023-03-30 15:25:33 +0300

    Increase limit distance to 30

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def cvt_to_2darray(numpy_ndarray, func):
 
 def compare_samples(ref_2darray, test_2darrays):
     ref_association = Associations(Sample(ref_2darray).convertToAngle())
-    test_1_association = Associations(sampleImage=Sample(test_2darrays[0]).convertToAngle(), usePlainDataframes=True, includeDistance=True, limitDistance=10)
-    test_2_association = Associations(sampleImage=Sample(test_2darrays[1]).convertToAngle(), usePlainDataframes=True, includeDistance=True, limitDistance=10)
+    test_1_association = Associations(sampleImage=Sample(test_2darrays[0]).convertToAngle(), usePlainDataframes=True, includeDistance=True, limitDistance=30)
+    test_2_association = Associations(sampleImage=Sample(test_2darrays[1]).convertToAngle(), usePlainDataframes=True, includeDistance=True, limitDistance=30)
 
     comp1 = ref_association.compare(test_1_association)
     comp2 = ref_association.compare(test_2_association)
